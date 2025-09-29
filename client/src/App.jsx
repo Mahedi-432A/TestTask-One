5fcee92f1feaaa93ac385fee2bec334e0f9b8d11
Mahedi-432A <mdmahedih038@gmail.com>
2025-09-29 17:39:16 +0600

    Refactor app structure by removing calendar component, adding routing, and creating Error and Home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-300 text-2xl text-center">Vite + React</h1>
-
-      <calendar-date class="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
-        <svg
-          aria-label="Previous"
-          className="fill-current size-4"
-          slot="previous"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
-        </svg>
-        <svg
-          aria-label="Next"
-          className="fill-current size-4"
-          slot="next"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
-        </svg>
-        <calendar-month></calendar-month>
-      </calendar-date>
-    </>
+    <Routes>
+      <Route path="/" element={<Home></Home>} />
+      <Route path="*" elemont={<Error></Error>} />
+    </Routes>
   );
 }
 
