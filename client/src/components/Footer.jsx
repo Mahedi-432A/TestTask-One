1e568da732fdc59cc983f8df523f8c05a9c7d4cb
Mahedi-432A <mdmahedih038@gmail.com>
2025-10-02 12:03:47 +0600

    Add social media icons and update footer component to display logo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import footerImage from '../assets/header.json'
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <div></div>
+        <div>
+          <img src={footerImage.logo} alt="Logo" />
+          <h3>Espresso Emporium</h3>
+          <p>Always ready to be your friend. Come & Contact with us to share your memorable moments, to share with your best companion.</p>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </div>
         <div></div>
       </div>
       <div></div>
