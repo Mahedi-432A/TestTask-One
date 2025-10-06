f35f7aa1907eb2f66116a7036d2102f78c3a609e
Mahedi-432A <mdmahedih038@gmail.com>
2025-10-07 01:08:10 +0600

    Refactor HomeComponents to improve layout and add product interaction features

--- client/src/components/HomeComponents.jsx
+++ client/src/components/HomeComponents.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import icons from "../assets/icons.json";
-import cups from '../assets/cups.json'
+import cups from "../assets/cups.json";
+import { Link } from "react-router-dom";
 
 const HomeComponents = () => {
   return (
@@ -26,27 +27,53 @@ const HomeComponents = () => {
       <section className="bg-[url('images/more/10.png')] bg-cover bg-center py-8 bg-no-repeat">
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-5 justify-items-center lg:w-4/6 md:w-3/4 w-10/12 mx-auto">
           <div>
-            <img className="lg:w-16 md:w-14 w-10" src={icons.icon1} alt="feature icon 1" />
-            <h2 className="mt-4 text-[#331A15] font-rancho lg:text-4xl md:text-3xl text-2xl">Awesome Aroma</h2>
+            <img
+              className="lg:w-16 md:w-14 w-10"
+              src={icons.icon1}
+              alt="feature icon 1"
+            />
+            <h2 className="mt-4 text-[#331A15] font-rancho lg:text-4xl md:text-3xl text-2xl">
+              Awesome Aroma
+            </h2>
             <p className="text-[#1B1A1A] mt-2 font-raleway md:text-sm lg:text-base text-xs">
               You will definitely be a fan of the design & aroma of your coffee
             </p>
           </div>
           <div>
-            <img className="lg:w-16 md:w-14 w-10" src={icons.icon2} alt="feature icon 2" />
-            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">High Quality</h2>
-            <p className="text-[#1B1A1A] mt-2 font-raleway md:text-sm lg:text-base text-xs">We served the coffee to you maintaining the best quality</p>
+            <img
+              className="lg:w-16 md:w-14 w-10"
+              src={icons.icon2}
+              alt="feature icon 2"
+            />
+            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">
+              High Quality
+            </h2>
+            <p className="text-[#1B1A1A] mt-2 font-raleway md:text-sm lg:text-base text-xs">
+              We served the coffee to you maintaining the best quality
+            </p>
           </div>
           <div>
-            <img className="lg:w-16 md:w-14 w-10" src={icons.icon3} alt="feature icon 3" />
-            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">Pure Grade</h2>
+            <img
+              className="lg:w-16 md:w-14 w-10"
+              src={icons.icon3}
+              alt="feature icon 3"
+            />
+            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">
+              Pure Grade
+            </h2>
             <p className="text-[#1B1A1A] mt-2 font-raleway md:text-sm lg:text-base text-xs">
               The coffee is made of the green coffee beans which you will love
             </p>
           </div>
           <div>
-            <img className="lg:w-16 md:w-14 w-10" src={icons.icon4} alt="feature icon 4" />
-            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">Proper Roasting</h2>
+            <img
+              className="lg:w-16 md:w-14 w-10"
+              src={icons.icon4}
+              alt="feature icon 4"
+            />
+            <h2 className="text-[#331A15] mt-4 font-rancho lg:text-4xl md:text-3xl text-2xl">
+              Proper Roasting
+            </h2>
             <p className="text-[#1B1A1A] mt-2 font-raleway md:text-sm lg:text-base text-xs">
               Your coffee is brewed by first roasting the green coffee beans
             </p>
@@ -56,19 +83,139 @@ const HomeComponents = () => {
 
       {/* products section */}
       <section className="text-center bg-[url('images/more/1.png')] bg-cover bg-center py-16 bg-white bg-no-repeat">
-        <p className="text-[#1B1A1A] lg:text-xl font-raleway">--- Sip & Savor ---</p>
-        <h1 className="text-[#331A15] font-rancho lg:text-5xl md:text-3xl text-2xl mt-2">Our Popular Products</h1>
-        <button className="rounded font-rancho lg:text-2xl text-xl px-4 mt-8 border-2 border-transparent hover:bg-[#F5F4F1] text-white hover:border-[#331A15] py-2 bg-[#E3B577]">Add Coffe</button>
+        <p className="text-[#1B1A1A] lg:text-xl font-raleway">
+          --- Sip & Savor ---
+        </p>
+        <h1 className="text-[#331A15] font-rancho lg:text-5xl md:text-3xl text-2xl mt-2">
+          Our Popular Products
+        </h1>
+        <Link to="/addCoffe">
+          <button className="rounded font-rancho lg:text-2xl text-xl px-4 mt-8 border-2 border-transparent hover:bg-[#F5F4F1] text-white hover:border-[#331A15] py-2 bg-[#E3B577]">
+            Add Coffe
+          </button>
+        </Link>
 
-        <div>
-          <div className="bg-[#F5F4F1] rounded-xl max-w-[640px]"></div>
+        <div className="mt-12 w-fit mx-auto text-start grid lg:grid-cols-2 grid-cols-1 gap-5">
+          <div className="bg-[#F5F4F1] rounded-xl max-w-xl w-fit p-7 flex items-center gap-4">
+            <div className="w-4/12">
+              <img src="images/1.png" alt="image of coffe" />
+            </div>
+            <div className="w-6/12 flex flex-col gap-3 font-raleway text-[#5C5B5B]">
+              <p>
+                <span className="text-[#1B1A1A]">Name: </span>Amerikano Coffe
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Cheif: </span>Mr. Martin Paul
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Price: </span>800 Taka
+              </p>
+            </div>
+            <div className="w-2/12 flex flex-col gap-3 text-white">
+              <button className="rounded font-rancho px-3 py-1 bg-[#D2B48C] cursor-pointer">
+                View
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#3C393B] cursor-pointer">
+                Edit
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#EA4744] cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+
+          <div className="bg-[#F5F4F1] rounded-xl max-w-xl w-fit p-7 flex items-center gap-4">
+            <div className="w-4/12">
+              <img src="images/1.png" alt="image of coffe" />
+            </div>
+            <div className="w-6/12 flex flex-col gap-3 font-raleway text-[#5C5B5B]">
+              <p>
+                <span className="text-[#1B1A1A]">Name: </span>Amerikano Coffe
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Cheif: </span>Mr. Martin Paul
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Price: </span>800 Taka
+              </p>
+            </div>
+            <div className="w-2/12 flex flex-col gap-3 text-white">
+              <button className="rounded font-rancho px-3 py-1 bg-[#D2B48C] cursor-pointer">
+                View
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#3C393B] cursor-pointer">
+                Edit
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#EA4744] cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+
+          <div className="bg-[#F5F4F1] rounded-xl max-w-xl w-fit p-7 flex items-center gap-4">
+            <div className="w-4/12">
+              <img src="images/1.png" alt="image of coffe" />
+            </div>
+            <div className="w-6/12 flex flex-col gap-3 font-raleway text-[#5C5B5B]">
+              <p>
+                <span className="text-[#1B1A1A]">Name: </span>Amerikano Coffe
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Cheif: </span>Mr. Martin Paul
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Price: </span>800 Taka
+              </p>
+            </div>
+            <div className="w-2/12 flex flex-col gap-3 text-white">
+              <button className="rounded font-rancho px-3 py-1 bg-[#D2B48C] cursor-pointer">
+                View
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#3C393B] cursor-pointer">
+                Edit
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#EA4744] cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
+
+          <div className="bg-[#F5F4F1] rounded-xl max-w-xl w-fit p-7 flex items-center gap-4">
+            <div className="w-4/12">
+              <img src="images/1.png" alt="image of coffe" />
+            </div>
+            <div className="w-6/12 flex flex-col gap-3 font-raleway text-[#5C5B5B]">
+              <p>
+                <span className="text-[#1B1A1A]">Name: </span>Amerikano Coffe
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Cheif: </span>Mr. Martin Paul
+              </p>
+              <p>
+                <span className="text-[#1B1A1A]">Price: </span>800 Taka
+              </p>
+            </div>
+            <div className="w-2/12 flex flex-col gap-3 text-white">
+              <button className="rounded font-rancho px-3 py-1 bg-[#D2B48C] cursor-pointer">
+                View
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#3C393B] cursor-pointer">
+                Edit
+              </button>
+              <button className="rounded font-rancho px-3 py-1 bg-[#EA4744] cursor-pointer">
+                Delete
+              </button>
+            </div>
+          </div>
         </div>
       </section>
 
       {/* gallery section */}
       <section className="bg-white text-center py-16 lg:py-20">
         <p className="text-[#1B1A1A] lg:text-xl font-raleway">Follow Us Now</p>
-        <h1 className="text-[#331A15] font-rancho lg:text-5xl md:text-3xl text-2xl mt-2">Folow on Instagram</h1>
+        <h1 className="text-[#331A15] font-rancho lg:text-5xl md:text-3xl text-2xl mt-2">
+          Folow on Instagram
+        </h1>
         <div className="mt-12 grid lg:grid-cols-4 grid-cols-2 gap-4 lg:w-4/6 md:w-3/4 w-10/12 mx-auto">
           <img src={cups.reactangle9} alt="gallery image 1" />
           <img src={cups.reactangle10} alt="gallery image 2" />
